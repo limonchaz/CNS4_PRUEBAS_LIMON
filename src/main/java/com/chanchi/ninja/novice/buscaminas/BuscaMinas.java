a8675b2e2395feb347039f2cbf3a892f25e2c0db
Francisco Limon <flimon@dsindigo.mx>
2018-02-08 19:09:15 -0600

    leyendo el archivo qu sirve como board

--- src/main/java/com/chanchi/ninja/novice/buscaminas/BuscaMinas.java
+++ src/main/java/com/chanchi/ninja/novice/buscaminas/BuscaMinas.java
@@ -1,12 +1,59 @@
 package com.chanchi.ninja.novice.buscaminas;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class BuscaMinas {
 
     public void startGame(String fileName) {
-        readFile(fileName);
+        try {
+            readFile(fileName);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
     
-    private void readFile(String fileName) {
+    public void readFile(String fileName) throws IOException {
+        FileReader fr = new FileReader(fileName);
+        BufferedReader br = new BufferedReader(fr);
         
+        String linea =  br.readLine();
+        
+        String[] rows = {};
+        
+        while(linea != null) {
+            int val = 0;
+            int left = 0;
+            int right = 0;
+            boolean flag = false;
+            rows = linea.split("");
+
+            for (int i = 0; i < rows.length; i++) {
+                String[] newRow = buildNumericBoard(rows);
+            }
+            System.out.println("");
+            linea = br.readLine();
+        }
+        
+        br.close();
+    }
+    
+    
+    public String[] buildNumericBoard(String[] row) {
+        String[] newRow = {};
+        for (int i = 0; i < row.length; i++) {
+            if (".".equals(row[i])) {
+                row[i] = "0";
+            }
+        }
+        return newRow;
+    }
+    
+    public void countMines(String val) {
+        if ("*".equals(val)) {
+            //obtener la posicion a la izquierda y la posicion a la derecha y sumarle un 1
+        }
     }
 }
--- src/test/java/com/chanchi/ninja/novice/buscaminas/BuscaMinasTest.java
+++ src/test/java/com/chanchi/ninja/novice/buscaminas/BuscaMinasTest.java
@@ -1,9 +1,18 @@
 package com.chanchi.ninja.novice.buscaminas;
 
+import org.junit.Test;
+
 public class BuscaMinasTest {
 
-    
-    public void testStartGame() {
+    @Test
+    public void testReadFile() {
+        String fileName = "/home/indigo30/git/CNS4_PRUEBAS_LIMON/src/main/resources/board.txt";
+        BuscaMinas bm = new BuscaMinas();
         
+        try {
+            bm.readFile(fileName);
+        }catch (Exception e) {
+            System.out.println("No encontro el archivo");
+        }
     }
 }
